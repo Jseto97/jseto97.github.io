7419609d71ccdef7b259a0017ba016cb90a2bb56
jseto97 <jsetomsc@gmail.com>
2019-01-21 01:20:08 -0500

    initial commit

---
+++ scripts/button.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+		$(document).delegate('.open', 'click', function(event){
+			$(this).addClass('oppenned');
+			event.stopPropagation();
+		})
+		$(document).delegate('body', 'click', function(event) {
+			$('.open').removeClass('oppenned');
+		})
+		$(document).delegate('.cls', 'click', function(event){
+			$('.open').removeClass('oppenned');
+			event.stopPropagation();
+		});
+	});
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+
+
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+
+
+    }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+$(window).scroll(function(){
+    $(".scroll").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+
+  $(window).scroll(function(){
+      $(".landing").css("opacity", 1 - $(window).scrollTop() / 1000);
+    });
