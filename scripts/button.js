656b91b5d10af4f0e3810b763a76d6f435f2ec79
jseto97 <jsetomsc@gmail.com>
2019-01-21 17:12:37 -0500

    td continued

--- scripts/button.js
+++ scripts/button.js
@@ -13,28 +13,27 @@ $(document).ready(function() {
 	});
 
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-
-
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-
-
-    }
-}
-
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction (), scrollFunction()};
+
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+				document.getElementById("myBtn").style.display = "block";
+
+
+		} else {
+				document.getElementById("myBtn").style.display = "none";
+
+
+		}
 }
 
 // When the user clicks on the button, scroll to the top of the document
